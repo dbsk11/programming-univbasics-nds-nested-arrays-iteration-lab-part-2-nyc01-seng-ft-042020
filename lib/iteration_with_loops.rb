464e5f095c29353a021cfc3dc490486c5ca140ee
Jessica K <jessikim807@gmail.com>
2020-04-09 00:34:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_min_in_nested_arrays(src)
   while row_index < src.count do
     element_index = 0
     shortest_string_element = " "
-    while element_index < src[row_index].count do 
+    while element_index < src[row_index].count do
       if src[row_index][element_index].length < shortest_string_element
         shortest_string_element = src[row_index][element_index]
       end
